a6121292fd9d3c93cab4f59821d3cb66815939a8
caochang <caochang@guazi.com>
2024-01-13 22:25:51 +0800

    feat: vite 编译处理

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        default: resolve(__dirname, 'index.html'),
+        v01: resolve(__dirname, 'example/day01/index.html'),
+      },
+      outDir: 'dist',
+    },
+  },
+  publicDir: 'public',
 })
